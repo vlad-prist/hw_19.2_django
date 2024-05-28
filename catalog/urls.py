f75f9b4a63ba626e8958cceeec95db8a3330d075
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-05-28 12:01:22 +0300

    Перенос настройки сеттингс между урлами.

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path
 from catalog.views import home, contacts
 from catalog.apps import CatalogConfig
@@ -9,4 +7,4 @@ new_app = CatalogConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
