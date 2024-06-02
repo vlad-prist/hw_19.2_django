c60c0a696e6c0db8ee7547e87c54de353f095f64
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-06-02 21:33:32 +0300

    hw_20.2

--- catalog/views.py
+++ catalog/views.py
@@ -12,7 +12,7 @@ def contacts(request):
     return render(request, 'catalog/contacts.html', context)
 
 
-def category_main_page(request):
+def category_main_page(request, ):
     category = Category.objects.all()
     context = {
         'category_list': category,
@@ -23,9 +23,12 @@ def category_main_page(request):
 
 def category_one(request, pk):
     category_one = get_object_or_404(Category, pk=pk)
+    prod_cat = category_one.products.all()
+
     context = {
-        'category_list': category_one,
+        'category_one': category_one,
         'title': f'{category_one.name}',
+        'prod_cat': prod_cat,
     }
     return render(request, 'catalog/category_one.html', context)
 
@@ -41,5 +44,10 @@ def products(request):
 
 def product_detail(request, pk):
     product_one = get_object_or_404(Product, pk=pk)
-    context = {'product_list': product_one, 'title': f'{product_one.name}'}
+
+    context = {
+        'product_one': product_one,
+        'title': f'{product_one.name}',
+
+    }
     return render(request, 'catalog/product_detail.html', context)
