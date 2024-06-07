15b933c6fd6fcfdeff7801db609c12ccd1d76a30
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-06-07 18:28:17 +0300

    Задание 1. контроллеры настроены по CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,8 +9,4 @@ urlpatterns = [
     path('', CategoryListView.as_view(), name='category_main_page'),
     path('catalog/<int:pk>/', CategoryDetailView.as_view(), name='category_one'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='products')
-    #path('products/<int:pk>/', products, name='products'),
-    #path('catalog/<int:pk>/', category_one, name='category_one'),
-    #path('', category_main_page, name='category_main_page'),
-    #path('products/<int:pk>/', product_detail, name='products'),
 ]
